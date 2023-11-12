c21d87d346fd7539f9754082f1803168285fe252
Daniel Adigun <105540876+Russoboy@users.noreply.github.com>
2023-11-12 21:38:34 +0100

    Update Image-slider.js
    Changed it to addEventListener

--- Image-slider.js
+++ Image-slider.js
@@ -1,23 +1,23 @@
 let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel img');
+const totalSlides = slides.length;
+
+document.querySelector('.prev').addEventListener('click', () => changeSlide(-1));
+document.querySelector('.next').addEventListener('click', () => changeSlide(1));
 
 function changeSlide(n) {
   currentIndex += n;
-  showSlide(currentIndex);
-}
-
-function showSlide(index) {
-  const slides = document.querySelectorAll('.slider img');
-
-  if (index < 0) {
-    currentIndex = slides.length - 1;
-  } else if (index >= slides.length) {
+  if (currentIndex < 0) {
+    currentIndex = totalSlides - 1;
+  } else if (currentIndex >= totalSlides) {
     currentIndex = 0;
   }
 
+  showSlide();
+}
+
+function showSlide() {
   slides.forEach((slide, i) => {
     slide.style.transform = `translateX(${(i - currentIndex) * 100}%)`;
   });
 }
-
-// Optional: Automatically advance slides
-// setInterval(() => changeSlide(1), 3000);
